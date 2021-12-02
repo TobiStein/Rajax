eaac7cdc384133d1cd6b39ede65f1a4753daf88d
Arthaudcom <arthaudmorin@gmail.com>
2021-12-02 20:13:31 +0100

    Header vides

--- src/layout/Header.svelte
+++ src/layout/Header.svelte
@@ -4,8 +4,6 @@
      src="./img/corsaire.png"
      alt="logo corsaire dunkerquois">
         <form>
-            <input type="text" placeholder="Recherchez">
-          <button type="button">🔎</button>
         </form>
         
       </main>
