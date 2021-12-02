236934d3e0a903bb7a6a80457fc7340081d0632e
Arthaudcom <arthaudmorin@gmail.com>
2021-12-02 20:13:16 +0100

    header vide

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,12 @@
   import { Router, Link, Route } from "svelte-routing";
   import Searchbar from "./layout/SearchBar.svelte";
   import Header from "./layout/Header.svelte";
+  import Personnes from "./layout/Personnes.svelte";
 
   export let url = "";
 </script>
 <Header />
 <Router url="{url}">
   <Route path="debug"><Searchbar /></Route>
+  <Route path="debug_arthaud"><Personnes /></Route>
 </Router>
