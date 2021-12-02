d18d01f50d7f7db535f9a2fcc7df5f22824066f4
Arthaudcom <arthaudmorin@gmail.com>
2021-12-02 20:58:52 +0100

    ajout tableaux

---
+++ src/layout/Personnes.svelte
@@ -0,0 +1,13 @@
+<table>
+    <tr>
+      <th>Noms</th>
+      <th>Prénoms</th>
+      <th>Date de Naissance</th>
+    </tr>
+    <tr>
+      <td>jean-Pierre</td>
+      <td>Polraneg</td>
+      <td>03/12/1982
+      </td>
+    </tr>
+</table>
