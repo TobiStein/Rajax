dacbb80de26d4305bebf0e1c02090e53adc6b191
Arthaudcom <arthaudmorin@gmail.com>
2021-12-02 22:03:29 +0100

    test aveugle

--- src/App.svelte
+++ src/App.svelte
@@ -4,12 +4,12 @@
   import Searchbar from "./layout/SearchBar.svelte";
   import Search from "./routes/Search.svelte";
   import Header from "./layout/Header.svelte";
-  import Personnes from "./layout/Personnes.svelte";
+  import EasterEgg from "./layout/EasterEgg.svelte";
   export let url = "";
 </script>
 <Header />
 <Router url="{url}">
   <Route path="debug"><Searchbar /></Route>
   <Route path="search/:query/:filters" let:params><Search query={params.query} filter={params.filters}/></Route>
-  <Route path="debug_arthaud"><Personnes /></Route>
+  <Route path="debug_arthaud"><EasterEgg /></Route>
 </Router>
