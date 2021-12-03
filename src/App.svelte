285317ece481bf5cf3b4e98a3ec6fc7bcd57cf81
Arthaudcom <arthaudmorin@gmail.com>
2021-12-03 01:08:12 +0100

    ChristmasEgg

--- src/App.svelte
+++ src/App.svelte
@@ -7,10 +7,12 @@
   import EasterEgg from "./layout/EasterEgg.svelte";
   import Personnes from "./layout/Personnes.svelte";
   import Article from "./elements/Article.svelte";
+  import Tibo from "./layout/Tibo.svelte";
   export let url = "";
 </script>
 <Header />
 <Router url="{url}">
+  <Route path="tiboaveugle"><Tibo /></Route>
   <Route path="debug"><Article text="# Je suis sympa non" /></Route>
   <Route path="search/:query/:filters" let:params><Search query={params.query} filter={params.filters}/></Route>
   <Route path="debug_arthaud"><EasterEgg /></Route>
