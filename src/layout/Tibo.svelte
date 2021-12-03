3042edaf4824e4a9a7a8f6777e3fc44e9bfb9627
Arthaudcom <arthaudmorin@gmail.com>
2021-12-03 02:12:18 +0100

    title

--- src/layout/Tibo.svelte
+++ src/layout/Tibo.svelte
@@ -1,7 +1,7 @@
 
 
 <p>Joyeuses fêtes</p>
-    <iframe width="800" height="600" src="https://www.youtube.com/embed/ucg3wvgk_Ac">  </iframe>
+    <iframe title="vidéo de noel de rick astley" width="800" height="600" src="https://www.youtube.com/embed/ucg3wvgk_Ac">  </iframe>
 
     <svelte:head>
     <style>
