42b44f957fc04c3142ae0b566c1ac56791ec501e
Arthaudcom <arthaudmorin@gmail.com>
2021-12-03 01:08:27 +0100

    chrchristmas egg

---
+++ src/layout/Tibo.svelte
@@ -0,0 +1,16 @@
+
+
+<p>Joyeuses fêtes</p>
+    <iframe width="800" height="600" src="https://www.youtube.com/embed/ucg3wvgk_Ac">  </iframe>
+
+    <svelte:head>
+    <style>
+        body {
+        background-image: url("img/sapin1.jpg");
+        }
+        header
+        {
+            background-image: url("img/sapin2.jpg");
+        }
+    </style>
+    </svelte:head>
